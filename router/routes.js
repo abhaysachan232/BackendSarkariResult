9a85250ee1f697ffa02a0c8635866d1d1ae649ee
abhaysachan232 <abhaysachan232@gmail.com>
2026-04-19 14:46:10 +0530

    op

--- router/routes.js
+++ router/routes.js
@@ -1,12 +1,11 @@
-const signIn = require("../Controllers/signIn");
-const signUp = require("../Controllers/signUp");
+const auth = require("../controller/authController");
 const router = require("express").Router();
 const User = require("../models/UserSchema");
 const express = require("express");
 const authMiddleware = require("../middleware/middleWare");
 
-router.post("/signup", signUp.signUp);
-router.post("/signin", signIn.signIn);
+router.post("/signup", auth.signUp);
+router.post("/signin", auth.signIn);
 router.get("/profile", authMiddleware, async (req, res) => {
   const UserId = req.user.id;
   const user = await User.findById(UserId);
