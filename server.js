fee4331eb56d1fd7d4f298bb07cee77f9dde2ddd
abhaysachan232 <abhaysachan232@gmail.com>
2026-04-19 15:36:15 +0530

    fix: add bcrypt

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express, { json } from "express";
 const connectDataBase = require("./config/db").default.default;
+import routes from "./router/routes";
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -9,7 +10,7 @@ app.use(json());
 app.get("/", (req, res) => {
   res.send("API running");
 });
-import routes from "./router/routes";
+
 app.use("/api", routes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
