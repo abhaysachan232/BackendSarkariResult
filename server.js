d6ccbc3f4a361155ff42d319528c2714d6fd19ee
abhaysachan232 <abhaysachan232@gmail.com>
2026-04-23 18:12:08 +0530

    op

--- server.js
+++ server.js
@@ -1,26 +1,11 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require ('express');
+const connectDataBase = require('./config/db');
 require('dotenv').config();
-const authRoutes = require('./routes/authRoutes');
-const auth = require('./controller/authController');
 
 const app = express();
-
-
+connectDataBase();
 app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+app.use("/", authRoutes);
 
-connectDB();
-
-// app.use("/api", authRoutes);
-app.get('/',(req,res)=>{
-  console.log("Hello World",req);
-  res.send("Hello World");
-})
-app.get("/api/health", (req, res) => {
-  res.send("OK HEALTH");
-});
-app.post("/api/login", auth.signIn);
-app.post("/api/signup", auth.signUp);
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
-});
+app.listen(process.env.PORT,()=>{console.log(`Server is running on port ${process.env.PORT}`)});
