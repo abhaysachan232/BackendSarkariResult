be357af7f648c345246feea62bc96f87941e50e1
abhaysachan232 <abhaysachan232@gmail.com>
2026-04-23 19:13:15 +0530

    cors

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require ('express');
 const connectDataBase = require('./config/db');
 require('dotenv').config();
+const cors = require('cors');
+
 
 const app = express();
 connectDataBase();
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "https://sarkariresult.rest",
+      "https://www.sarkariresult.rest",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
 const authRoutes = require("./routes/authRoutes");
 app.use("/", authRoutes);
 
